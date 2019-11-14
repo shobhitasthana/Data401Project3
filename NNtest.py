e510ea58ba6d40f779011fa1ecacd924878e8991
Shobhit Asthana <shobhit.asthana@yahoo.com>
2019-11-13 22:28:07 -0800

    changed forward prop to have y-hat as scalar

--- NNtest.py
+++ NNtest.py
@@ -1,14 +1,16 @@
 from NeuralNetwork import *
 
 nodes = [3,2,2,1]
-activation = ['relu','relu','relu','relu']
+activation = ['relu','sigmoid','sigmoid','relu']
 layers = len(nodes) - 1
 net = NeuralNetwork(nodes,activation)
 net.initialize_net()
 data = np.matrix([1,-2,2])
 net.parameter_dict[0]['h'] = data
-net.parameter_dict[1]['w'] = np.matrix([[0,0],[3.,1],[2,0],[1,-1]])
-net.parameter_dict[2]['w'] = np.matrix([[0,0],[-2.,4],[0,-3]])
-net.parameter_dict[3]['w'] = np.matrix([0,5.,2]).transpose()
+net.parameter_dict[1]['w'] = np.matrix([[0,0],[3.,1.],[2.,0],[1,-1]])
+net.parameter_dict[2]['w'] = np.matrix([[0,0],[-2.,4],[0,-3.]])
+net.parameter_dict[3]['w'] = np.matrix([0,5.,2])
 
-net.forward_propogate(data)
+print(net.forward_propogate(data))
+
+print(net.parameter_dict)
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -74,10 +74,33 @@ class NeuralNetwork:
             self.parameter_dict[i]['z'] = newz
             self.parameter_dict[i]['h'] = np.append(1,newh)
             print('z',newz, 'h',newh )
-
-        self.parameter_dict['y_hat'] = np.matmul(self.parameter_dict[len(self.Nodes)-2]['h'],self.parameter_dict[len(self.Nodes)-1]['w'].transpose())
+        print(np.asscalar(np.matmul(self.parameter_dict[len(self.Nodes)-2]['h'],self.parameter_dict[len(self.Nodes)-1]['w'].transpose())))
+        self.parameter_dict['y_hat'] = np.asscalar(np.matmul(self.parameter_dict[len(self.Nodes)-2]['h'],self.parameter_dict[len(self.Nodes)-1]['w'].transpose()))
 
         return self.parameter_dict['y_hat']
+
+    def calculate_deltas(self,true):
+        """
+        true: the value of the true y-value
+        assumes that all the entries in the dictionary are np.array where applicable
+        assumes weights matrix for w_ij appears as j being constant across rows and i across cols
+        e.g. [[w11,w21],[w12,w22]] 
+        """
+        # calculate sum of dervatives of cost
+        error = self.parameter_dict["y_hat"] - true
+        # last layer d (indexing is correct?)
+        # get Zs from the last layer as well
+        # error * g'(z)
+        self.parameter_dict[len(self.Nodes) - 1]["delta"] = error * np.array([self.activate_prime(x) for 
+                                                               x in self.parameter_dict[len(self.Nodes) - 1]["z"]])
+        # last layer is special case, now loop through all the previous layers to calculate sets of deltas
+        # from the second to last layer to the first layer – backwards
+        for i in range(len(self.Nodes) - 2,-1,-1):
+            # delta = weights.T x diag(g'(z)) x delta[i+1]
+            # extra Ts are just making things into column vectors
+            g_prime_layer = np.array([self.activate_prime(x) for x in self.parameter_dict[i]["z"]]).T
+            self.parameter_dict[i]["delta"] = self.parameter_dict[i]["w"].T.dot(
+                np.diag(g_prime_layer)).dot(self.parameter_dict[i + 1]["delta"].T)
 
 
 
